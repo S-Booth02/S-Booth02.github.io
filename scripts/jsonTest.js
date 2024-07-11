e498fa5a8d220308ea0fd1612b356ac1fd5cb7fd
S-Booth02 <167040341+S-Booth02@users.noreply.github.com>
2024-07-11 14:21:07 -0500

    Update jsonTest.js

--- scripts/jsonTest.js
+++ scripts/jsonTest.js
@@ -8,4 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
       document.querySelector(".testJson h1").innerHTML =json.buildingList[0].buildingName;
       document.querySelector(".testJson p").innerHTML =json.buildingList[0].buildingNumber;
     })
+  .then(createEle())
 });
+function createEle(){
+      for (let i = 0; i < 5; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.id = concat("group-marker-", i);
+        newDiv.class = "marker-all marker-group";
+        newDiv.style = "display:block; transform:scale(1); left:50%; top:20%;";
+        newDiv.onclick  = "";
+        var newA =  document.createElement("a");
+        newA.href = "javascript:void(0);";
+        newA.aria-label = "Admin Complex"
+        var newSpan = document.createElement("span");
+        newSpan.textContent = "test";
+        newDiv.appendChild(newA);
+        newDiv.appendChild(newSpan);
+        var space = document.getElementById("map-image");
+        document.body.insertAfter(newDiv,space);
+      }
+    console.log("New Elements Created")
+}
