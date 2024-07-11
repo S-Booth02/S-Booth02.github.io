be80f3c52e8e7bbf1a01570c6931d8091fd2ec8a
S-Booth02 <167040341+S-Booth02@users.noreply.github.com>
2024-07-11 10:43:27 -0500

    Update jsonTest.js

--- scripts/jsonTest.js
+++ scripts/jsonTest.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     .then((json) => {
       console.log(json);
 
-      document.querySelector(".testJson h1").innerHTML =buildingList.buildingName[0];
+      document.querySelector(".testJson h1").innerHTML =json.buildingList[0].buildingName[0];
       document.querySelector(".testJson p").innerHTML =json[0].buildingNumber;
     })
 });
