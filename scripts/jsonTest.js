2d610b9d869edca6d3e0d43cb2ca0a7458af1d64
S-Booth02 <167040341+S-Booth02@users.noreply.github.com>
2024-07-11 09:34:07 -0500

    Add files via upload

---
+++ scripts/jsonTest.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('../buildingDatabase.json')
+    .then((response)=>response.json())
+    .then((json) => {
+      console.log(json);
+
+      document.querySelector(".testJson h1").innerHTML =json[0].buildingName;
+      document.querySelector(".testJson p").innerHTML =json[0].buildingNumber;
+    })
+});
