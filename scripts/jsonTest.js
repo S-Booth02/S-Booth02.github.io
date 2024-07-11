06dffe0f7b799e4accd55cd33ad9ef9313dc9b3c
S-Booth02 <167040341+S-Booth02@users.noreply.github.com>
2024-07-11 13:04:12 -0500

    Update jsonTest.js

--- scripts/jsonTest.js
+++ scripts/jsonTest.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   fetch('../buildingDatabase.json')
     .then((response)=>response.json())
-    .then((json) => json.filter(item => item.active === true))
     .then((json) => {
       console.log(json);
+
 
       document.querySelector(".testJson h1").innerHTML =json.buildingList[0].buildingName;
       document.querySelector(".testJson p").innerHTML =json.buildingList[0].buildingNumber;
