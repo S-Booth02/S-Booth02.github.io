abd4234d8c59f26ed519296c485fc29f21b25e24
S-Booth02 <167040341+S-Booth02@users.noreply.github.com>
2024-07-18 08:33:57 -0500

    Update screen_styles.cs
    adding media declarations for other screen sizes

--- styles/screen_styles.cs
+++ styles/screen_styles.cs
@@ -1,1 +1,37 @@
-
+/*header*/
+@media (min-width: 600px) {
+  .site_header {
+    padding:1em 0 0 1.2em;
+  }
+}
+@media (min-width: 769px) {
+  .site_header {
+    padding: 1.1em 0 0  1.5em;
+  }
+}
+@media (min-width:  1024px) {
+  .site_header {
+    padding: 1.1em 0 0 2em;
+  }
+}
+/*body*/
+@media (min-width: 600px) {
+  .body {
+    font-size: 98%;
+  }
+}
+@media (min-width: 769px) {
+  .body {
+    font-size: 100%;
+  }
+}
+@media (min-width:  1024px) {
+  .body {
+    font-size: 102%;
+  }
+}
+@media (min-width:  1200px) {
+  .body {
+    font-size: 104%;
+  }
+}
